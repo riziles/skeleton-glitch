4562f20073cdf87e6cb7bb44b96722d254961c1e
Michael Riley <michaelriley@situsamc.com>
2023-01-29 23:57:57 -0700

    cleaner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,27 @@
 <script>
-	import { Modal, modalStore } from '@skeletonlabs/skeleton';
+  import { Modal, modalStore } from '@skeletonlabs/skeleton';
 
-	async function triggerAlert() {
-		const alert = {
-			type: 'alert',
-			title: 'Example Alert',
-			body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at mauris ut nisl molestie eleifend ac a neque. Nunc sit amet malesuada orci. Sed id consequat neque. Pellentesque vel urna varius, fringilla odio et, congue diam. <br /><br /><a href="https://google.com">href</a>`,
-			buttonTextCancel: 'Cancel'
-		};
-		await modalStore.trigger(alert);
-		document.querySelector('.modal-body').scrollTop = 0
-	}
+  async function triggerAlert() {
+    const alert = {
+      type: 'alert',
+      title: 'Example Alert',
+      body: `Lorem ipsum dolor sit amet, consectetur 
+        adipiscing elit. Curabitur at mauris ut nisl molestie 
+        eleifend ac a neque. Nunc sit amet malesuada orci. Sed 
+        id consequat neque. Pellentesque vel 
+        urna varius, fringilla odio et, congue diam. 
+        <br /> <br /> 
+        <a href="https://google.com">href</a>`,
+      buttonTextCancel: 'Cancel'
+    };
+  // workaround for scroll issue:
+    await modalStore.trigger(alert);
+    document.querySelector('.modal-body').scrollTop = 0
+  }
 </script>
 
-<Modal regionBody	='max-h-24 overflow-auto' width = 'max-w-xs'/>
+<Modal regionBody = 'max-h-24 overflow-auto' width = 'max-w-xs'/>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<button class="btn variant-filled-primary btn-base" on:click={triggerAlert}> Open Modal </button>
+  <button class="btn variant-filled-primary btn-base" on:click={triggerAlert}> Open Modal </button>
 </div>
